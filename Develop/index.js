e4a0b601974004123d9e9b45d56263f16b7003c5
Kyle <kylemphillips65@yahoo.com>
2023-01-03 20:08:23 -0500

    set up prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,32 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./src/page-template');
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const pickManger = () => {
+    return inquirer.createPromptModule([
+        {
+            type: "input",
+            message: "Mangers Name",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Managers ID number",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "Managers Email",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "Managers Office Number",
+            name: "phoneNumber",
+        },
+    ])
+}
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,17 @@
 const Employee = require("./Employee");
 
-class Engineer extends Employee {}
+class Engineer extends Employee {
+    
+};
+
+return Employee.prompt([
+        {
+            type: "input",
+            message: "Id",
+            message: "What is your E-mail?",
+            message: "What is Your GitHub?",
+            name: "Engineer"
+        }
+]);
 
 module.exports = Engineer;
