9c30ccfd46f35ae0b79308035908ae53cee84ae5
Kyle <kylemphillips65@yahoo.com>
2023-01-04 16:39:14 -0500

    passing test

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -10,6 +10,8 @@ describe("Employee", () => {
         expect(typeof employee).toBe('object')
     });
 
+
+
     it("should take in a name and assign to an id key", () => {
         const name = 'kyle';
         const employee = new Employee(name);
@@ -17,21 +19,51 @@ describe("Employee", () => {
         expect(employee.name).toBe('kyle');
     });
 
-    it("should take in a nemae that is accessible with the getName method", () => {
+    it("Should take in a name that is accessible with the getName method", () => {
         const name = 'kyle';
         const employee = new Employee(name);
 
         expect(employee.getName()).toBe('kyle');
     });
 
+
+
     it("should take in an id and assign to an id key", () => {
         const name = 'kyle';
         const id = 1023;
-        const employee = new Employee(name, id,)
+        const employee = new Employee(name, id)
 
         expect(employee.id).toBe(1023);
     });
     
+    it("Should take in a id that is accessible with the getId method", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const employee = new Employee(name, id)
+        
+        expect(employee.getId()).toBe(1023);
+    });
+
+
+
+    it("should take in an email and assign to an id key", () => { 
+         const name = 'kyle';
+         const id = 1023;
+         const email = 'kylephillips65@yahoo.com';
+         const employee = new Employee(name, id, email)
+
+         expect(employee.email).toBe('kylephillips65@yahoo.com');
+    });
+
+    it("Should take in a email that is accessible with the getEmail method", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const employee = new Employee(name, id, email) 
+
+        expect(employee.getEmail()).toBe('kylephillips65@yahoo.com')
+    });
+
 
     
 });
--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -1,7 +1,68 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Engineer has a github account", () => {
-    const engineer = new Engineer ('Kyle Phillips', 1023, 'kylephillips65@yahoo.com', 'KMPhillips20');
+describe("Engineer", () => {
+    it("should create a class called Engineer which return a type of object", () => {
+        const name = 'melissa';
+        const id = 1024;
+        const email = 'melphillips66@yahoo.com';
+        const engineer = new Engineer(name, id, email)
 
-    expect(engineer.github).toEqual(expect.any(String));
+        expect(typeof employee).toBe('object')
+    });
+
+
+    it("should take in a name and assign to an id key", () => {
+        const name = 'melissa';
+        const engineer = new Engineer(name);
+
+        expect(engineer.name).toBe('melissa');
+    });
+
+    it("Should take in a name that is accessible with the getName method", () => {
+        const name = 'melissa';
+        const engineer = new Engineer(name);
+
+        expect(engineer.getName()).toBe('melissa')
+    });
+
+
+
+    it("should take in an id and assign to an id key", () => {
+        const name = 'melissa';
+        const id = 1024;
+        const engineer = new Engineer(name, id)
+
+        expect(engineer.id).toBe(1024);
+    });
+
+    it("Should take in a id that is accessible with the getId method", () => {
+        const name = 'melissa';
+        const id = 1024;
+        const engineer = new Engineer(name, id)
+        
+        expect(engineer.getId()).toBe(1023);
+    });
+
+
+    it("should take in an email and assign to an id key", () => { 
+        const name = 'melissa';
+        const id = 1024;
+        const email = 'melphillips66@yahoo.com';
+        const engineer = new Engineer(name, id, email)
+
+        expect(engineer.email).toBe('melphillips66@yahoo.com');
+   });
+
+   it("Should take in a email that is accessible with the getEmail method", () => {
+       const name = 'melissa';
+       const id = 1024;
+       const email = 'melphillips66@yahoo.com';
+       super(name, id, email);
+       const engineer = new Engineer(name, id, email) 
+
+       expect(engineer.getEmail()).toBe('melphillips66@yahoo.com')
+   });
+
+
+    
 });
