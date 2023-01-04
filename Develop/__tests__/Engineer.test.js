b837eceb809b277f4e6450668d8a5e06b75e8f4f
Kyle <kylemphillips65@yahoo.com>
2023-01-04 17:27:52 -0500

    got 3 out of 4 test to pass

--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -2,42 +2,43 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
     it("should create a class called Engineer which return a type of object", () => {
-        const name = 'melissa';
-        const id = 1024;
-        const email = 'melphillips66@yahoo.com';
-        const engineer = new Engineer(name, id, email)
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const gitHub = 'KMPhillips20';
+        const engineer = new Engineer(name, id, email, gitHub)
 
-        expect(typeof employee).toBe('object')
+        expect(typeof engineer).toBe('object')
     });
 
 
     it("should take in a name and assign to an id key", () => {
-        const name = 'melissa';
+        const name = 'kyle';
         const engineer = new Engineer(name);
 
-        expect(engineer.name).toBe('melissa');
+        expect(engineer.name).toBe('kyle');
     });
 
     it("Should take in a name that is accessible with the getName method", () => {
-        const name = 'melissa';
+        const name = 'kyle';
         const engineer = new Engineer(name);
 
-        expect(engineer.getName()).toBe('melissa')
+        expect(engineer.getName()).toBe('kyle')
     });
 
 
 
     it("should take in an id and assign to an id key", () => {
-        const name = 'melissa';
-        const id = 1024;
+        const name = 'kyle';
+        const id = 1023;
         const engineer = new Engineer(name, id)
 
-        expect(engineer.id).toBe(1024);
+        expect(engineer.id).toBe(1023);
     });
 
     it("Should take in a id that is accessible with the getId method", () => {
-        const name = 'melissa';
-        const id = 1024;
+        const name = 'kyle';
+        const id = 1023;
         const engineer = new Engineer(name, id)
         
         expect(engineer.getId()).toBe(1023);
@@ -45,23 +46,45 @@ describe("Engineer", () => {
 
 
     it("should take in an email and assign to an id key", () => { 
-        const name = 'melissa';
-        const id = 1024;
-        const email = 'melphillips66@yahoo.com';
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
         const engineer = new Engineer(name, id, email)
 
-        expect(engineer.email).toBe('melphillips66@yahoo.com');
+        expect(engineer.email).toBe('kylephillips65@yahoo.com');
    });
 
    it("Should take in a email that is accessible with the getEmail method", () => {
-       const name = 'melissa';
-       const id = 1024;
-       const email = 'melphillips66@yahoo.com';
-       super(name, id, email);
+       const name = 'kyle';
+       const id = 1023;
+       const email = 'kylephillips65@yahoo.com';
        const engineer = new Engineer(name, id, email) 
 
-       expect(engineer.getEmail()).toBe('melphillips66@yahoo.com')
+       expect(engineer.getEmail()).toBe('kylephillips65@yahoo.com')
    });
+
+
+
+   it("should take in an gitHub and assign to an id key", () => { 
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const gitHub = 'KMPhillips20'
+        const engineer = new Engineer(name, id, email, gitHub)
+
+        expect(engineer.gitHub).toBe('https://github.com/KMPhillips20/');
+    });
+
+    it("Should take in a email that is accessible with the getHub method", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips@yahoo.com';
+        const gitHub = 'KMPhillips20'
+        const engineer = new Engineer(name, id, email, gitHub) 
+
+   expect(engineer.getGitHub()).toBe('https://github.com/KMPhillips20/')
+});
+
 
 
     
--- Develop/__tests__/Intern.test.js
+++ Develop/__tests__/Intern.test.js
@@ -1,8 +1,92 @@
+const Employee = require("../lib/Employee");
 const Intern = require("../lib/Intern");
 
-describe("Interns are still in school", () => {
-    const intern = new Intern('Kyle Phillips', 1023, 'kylephillips65@yahoo.com', 'NC State');
+describe("Interns", () => {
+    it("should create a class called Employee which return a type of object", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const school = 'NCSU';
+        const intern = new Intern(name, id, email,school)
 
-    expect(engineer.gitHub).toEqual(expect.any(String));
+        expect(typeof intern).toBe('object')
+    });
+
+
+
+    it("should take in a name and assign to an id key", () => {
+        const name = 'kyle';
+        const intern = new Intern(name);
+
+        expect(intern.name).toBe('kyle');
+    });
+
+    it("Should take in a name that is accessible with the getName method", () => {
+        const name = 'kyle';
+        const intern = new Intern(name);
+
+        expect(intern.getName()).toBe('kyle');
+    });
+
+
+
+    it("should take in an id and assign to an id key", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const intern = new Intern(name, id)
+
+        expect(intern.id).toBe(1023);
+    });
+    
+    it("Should take in a id that is accessible with the getId method", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const intern = new Intern(name, id)
+        
+        expect(intern.getId()).toBe(1023);
+    });
+
+
+
+    it("should take in an email and assign to an id key", () => { 
+         const name = 'kyle';
+         const id = 1023;
+         const email = 'kylephillips65@yahoo.com';
+         const intern = new Intern(name, id, email)
+
+         expect(intern.email).toBe('kylephillips65@yahoo.com');
+    });
+
+    it("Should take in a email that is accessible with the getEmail method", () => {
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const intern = new Intern(name, id, email) 
+
+        expect(intern.getEmail()).toBe('kylephillips65@yahoo.com')
+    });
+
+    
+    it("should take in a school and assign to an id key", () => { 
+        const name = 'kyle';
+        const id = 1023;
+        const email = 'kylephillips65@yahoo.com';
+        const school = 'NCSU';
+        const intern = new Intern(name, id, email, school)
+
+        expect(intern.school).toBe('NCSU');
+   });
+
+   it("Should take in a school that is accessible with the getSchool method", () => {
+       const name = 'kyle';
+       const id = 1023;
+       const email = 'kylephillips65@yahoo.com';
+       const school = 'NCSU';
+       const intern = new Intern(name, id, email, school) 
+
+       expect(intern.getSchool()).toBe('NCSU')
+   });
+
+
 
 });
