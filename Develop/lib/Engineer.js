e00899834e70b7154b13c41e0117af3a73776ef0
Kyle <kylemphillips65@yahoo.com>
2023-01-04 09:40:00 -0500

    worked on all the js files

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,9 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    
-};
-
+        constructor(name, id, email, gitHub) {
+            super(name, id, email);
+            this.gitHub = 'https://github.com/' + gitHub + '/';
+        }
+    }
 module.exports = Engineer;
