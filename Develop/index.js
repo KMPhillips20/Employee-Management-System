cd73c183d408dad3c60cc0511e46214cc1b132f1
Kyle <kylemphillips65@yahoo.com>
2023-01-10 16:43:43 -0500

    reset up my prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,141 +6,69 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-const teamArray = [];
 
-const employeeBio =[
-        {
-            type: "list",
-            message: "What is your position?",
-            name: "position",
-            choices: ["Manager", "Engineer", "Intern"],
-            
-        },
-        {
-            type: "input",
-            message: "Employee name?",
-            name: "name",
-            validate: Position => {
-                if (Position) {
-                    return true;
-                } else {
-                    console.log("Must enter Employee name.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "Employee email?",
-            name: "email",
-            validate: email => {
-                if (email) {
-                    return true;
-                } else {
-                    console.log("Must enter Employee email.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "Employee ID?",
-            name: "id",
-            validate: id => {
-                if (id) {
-                    return true;
-                } else {
-                    console.log("Must enter Employee ID.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "Managers office number?",
-            name: "officeNumber",
-            when: (officeNumber) => officeNumber.role === "Manager",
-            validate: officeNumber => {
-                if (officeNumber) {
-                    return true;
-                } else {
-                    console.log("Must enter Managers Office Number.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "Employee GitHub account?",
-            name: "gitHub",
-            when: (gitHub) => gitHub.role === "Engineer",
-            validate: gitHub => {
-                if (gitHub) {
-                    return true;
-                } else {
-                    console.log("Must enter Engineer Git Hub Account.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "What school or college do you attend?",
-            name: "school",
-            when: (school) => school.role === "Intern",
-            validate: school=> {
-                if (school) {
-                    return true;
-                } else {
-                    console.log("Must enter your Education Background.");
-                    return false; 
-                }
-            }
-        },
-        {
-            type: "confirm",
-            name: "add",
-            message: "Do you want to add anymore team members to your company?",
-            default: false,
-        }
-    ];
+const prompt = inquirer.createPromptModule();
+const crewMembers = [];
 
+const managerPrompts = [
+    {
+        message: 'Manager\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'Manager\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'Manager\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'Manager\'s Office Number?',
+        name: 'officeNumber',
+    },
+];
 
+const engineerPrompts = [
+    {
+        message: 'Engineer\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'Engineer\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'Engineer\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'Engineer\'s GitHub?',
+        name: 'gitHub',
+    },
+];
 
-const addEmployee = () => {
-    return inquirer.prompt(employeeBio)
-    .then(employeeData => {
-        let { position, name, email, id, officeNumber, gitHub, school} = answers;
-        let employee;
-        if (position === "Manager") {
-            employee = new Manager(name, email, id, officeNumber)
-        }
-        if (position === "Engineer") {
-            employee = new Engineer(name, email, id, gitHub)
-        }
-        if (position === "Intern") {
-            employee = new Intern(name, email, id, school)
-        }
-        teamArray.push(employee);
+const internPrompts = [
+    {
+        message: 'Intern\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'Intern\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'Intern\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'Intern\'s school?',
+        name: 'school',
+    },
+];
 
-        if (answers.confirmAddEmployee) {
-            return addEmployee();
-        } else {
-            const page = generatePage(teamArray);
-            writeFile(page);
-        }
-    });
-};
-
-const writeFile = data => {
-    fs.writeFile("./dist/team.html", data, err => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Get to know your teammates")
-        }
-    })
-};
-
-
-addEmployee()
+prompt(managerPrompts)
+    .then((info) => {
+        const manager = new Manager(info.name, info.id, info.email, info.officeNumber)
+        crewMembers.push(manager);
+});
