17221d6b37990d77c2a4b70b148cbc9bf3684dc5
Kyle <kylemphillips65@yahoo.com>
2023-01-10 19:24:51 -0500

    got all my prompts returing the questions for the all the employees positions in my terminal

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,7 @@ const generatePage = require('./src/page-template');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 
 
 const prompt = inquirer.createPromptModule();
@@ -69,6 +70,49 @@ const internPrompts = [
 
 prompt(managerPrompts)
     .then((info) => {
-        const manager = new Manager(info.name, info.id, info.email, info.officeNumber)
+        const manager = new Manager(info.name, info.id, info.email, info.officeNumber);
         crewMembers.push(manager);
+})
+.then(() => {
+    return prompt({
+        message: "WOULD YOU LIKE TO ADD ANOTHER MEMBER TO THE CREW?",
+        type: "confirm",
+        name: "addMember",
+    })
+})
+.then(({ addMember }) => {
+    if (addMember) {
+        console.log("YOU GOT IT DUDE!");
+    } else {
+        console.log("Write it up");
+    }
+})
+.then(() => {
+    return prompt({
+        type: "rawlist",
+        message: "What employee position would you like to add to the crew?",
+        choices: [
+            "Manager", 
+            "Engineer", 
+            "Intern",
+        ],
+        name: "type"
+    })
+})
+.then(({ type }) => {
+    switch(type) {
+        case "Manager": {
+            return prompt(managerPrompts);
+        }
+        case "Engineer": {
+            return prompt(engineerPrompts);
+        }
+        case "Intern": {
+            return prompt(internPrompts);
+        }
+    }
+})
+.then((employee) => {
+    console.log(employee);
 });
+
